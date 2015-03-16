138fa4616666d1f68a7e027784de8edafb83fda2
hanu <hanu1307@gmail.com>
2015-03-17 00:11:09 +0530

    first coomit

---
+++ MvcApplication1/MvcApplication1/Controllers/testController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class testController : Controller
+    {
+        //
+        // GET: /test/
+
+        public string Index() 
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        } 
+
+    }
+}
